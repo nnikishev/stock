323d66b41191a1ae008454523eda6fb0ee2e9388
Никишев Николай Сергеевич <n.nikishev@comita.digital>
2024-10-14 12:42:39 +0700

    cors

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ from os import getenv
 
 
 DEBUG = getenv("DEBUG", True)
-LOGGER_NAME = getenv("LOGGER_NAME", "schema_editor")
+LOGGER_NAME = getenv("LOGGER_NAME", "stock_app")
 LOG_LEVEL = getenv("LOG_LEVEL", "DEBUG" if DEBUG else "ERROR")
-CORS_ORIGIN_WHITELIST = ["http://localhost:3000", "http://194.87.213.123"]
+CORS_ORIGIN_WHITELIST = ["http://194.87.213.123", "http://localhost:3000"]
 
 LOGGING = {
     "version": 1,
